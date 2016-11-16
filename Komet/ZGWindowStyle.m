184f5c83e5c2945c4a626335e838905280ae9542
Zorg <zorgiepoo@gmail.com>
2016-11-15 22:42:36 -0500

    Make comment color stand out a little more in Default theme
    This uses the same color that the master branch uses.

--- Komet/ZGWindowStyle.m
+++ Komet/ZGWindowStyle.m
@@ -40,7 +40,7 @@
 			 material:NSVisualEffectMaterialLight
 			 textColor:[NSColor colorWithDeviceRed:0.0 green:0.0 blue:0.0 alpha:1.0]
 			 textHighlightColor:nil
-			 commentColor:[NSColor colorWithDeviceRed:0.0 green:0.0 blue:0.0 alpha:0.7]
+			 commentColor:[NSColor darkGrayColor]
 			 overflowColor:[NSColor colorWithDeviceRed:1 green:0.690 blue:0.231 alpha:0.5]
 			 fallbackBackgroundColor:[NSColor colorWithDeviceRed:1.0 green:1.0 blue:1.0 alpha:0.95]
 			 scrollerKnobStyle:NSScrollerKnobStyleDark];
