2e15a18e2b5c7e0c00b72bf26c89ada819199d21
Zorg <zorgiepoo@gmail.com>
2020-11-15 13:06:58 -0800

    Fix indentation of KometUITests

--- KometUITests/KometUITests.swift
+++ KometUITests/KometUITests.swift
@@ -184,32 +184,31 @@ class KometApp {
 }
 
 class KometUITests: XCTestCase {
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+		// In UI tests it is usually best to stop immediately when a failure occurs.
+		continueAfterFailure = false
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+		// In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+	}
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
 
 	// MARK: Simple
 	
-    func testNewCommit() throws {
+	func testNewCommit() throws {
 		let app = try KometApp(filename: "new-commit")
-		
+	
 		let newContent = "Hello there"
 		app.typeText(newContent)
-		
+	
 		let (breadcrumbs, finalContent) = try app.commit()
 		XCTAssertEqual(breadcrumbs!.exitStatus, 0, "commit failed with non-zero status")
 		XCTAssertEqual(finalContent, newContent + app.initialContent)
-    }
+	}
 	
 	func testCanceledNewCommit() throws {
 		let app = try KometApp(filename: "new-commit")
