b74cedba81805f9c2999d2d6e746145145bb1c13
Zorg <zorgiepoo@gmail.com>
2025-06-17 20:54:11 -0700

    Increase top bar height

--- Komet/EditorWindowController.swift
+++ Komet/EditorWindowController.swift
@@ -380,12 +380,6 @@ enum VersionControlType {
 	private func updateCurrentStyle() {
 		// Style top bar
 		do {
-#if canImport(AppKit, _version: "2665.8")
-			if #available(macOS 26, *) {
-				topBar.prefersCompactControlSizeMetrics = true
-			}
-#endif
-			
 			topBar.wantsLayer = true
 			topBar.layer?.backgroundColor = style.barColor.cgColor
 			
